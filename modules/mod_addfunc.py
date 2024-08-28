fe510a561ae09aa35a6bad2a1b77e0007ab6cfc4
olasupo.o <olasupo.o@gmail.com>
2024-08-28 18:50:03 +0100

    modules and packages codes added

---
+++ modules/mod_addfunc.py
@@ -0,0 +1,17 @@
+# Example - Simple function 1
+
+# Define a function 'addition' that takes two arguments 'a' and 'b'
+def addition(a, b):
+    """
+    This function takes two numbers 'a' and 'b' as input and returns their sum.
+    """
+    result = a + b  # Calculate the sum of 'a' and 'b'
+    return result  # Return the result of the addition
+
+# Define a function 'subtraction' that takes two arguments 'a' and 'b'
+def subtraction(a, b):
+    """
+    This function takes two numbers 'a' and 'b' as input and returns their difference.
+    """
+    result = a - b  # Calculate the difference between 'a' and 'b'
+    return result  # Return the result of the subtraction
