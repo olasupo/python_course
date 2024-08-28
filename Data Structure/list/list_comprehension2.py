10933fdd29193e29e6ea7fa46668e7e819bdf8d8
olasupo.o <olasupo.o@gmail.com>
2024-08-28 15:57:05 +0100

    Comment added

--- Data Structure/list/list_comprehension2.py
+++ Data Structure/list/list_comprehension2.py
@@ -1,9 +1,13 @@
-#Example - using list comprehensions...
-a = [1,5,2]
+# Example - using list comprehensions to conditionally extend a list
 
-b = [1,4,6]
+# Initializing two lists, 'a' and 'b'
+a = [1, 5, 2]
+b = [1, 4, 6]
 
+# Using list comprehension to extend list 'a' with list 'b' 
+# The comprehension runs for a single iteration (range(1)) 
+# and only extends 'a' if 'a' is not equal to 'b'
 [a.extend(b) for i in range(1) if a != b]
 
+# Printing the modified list 'a'
 print(a)
-
